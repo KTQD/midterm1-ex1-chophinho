888109584d169468c1a84fd8d23a38da28f16dc0
Ngo Ha Phuong <121754520+chophinho@users.noreply.github.com>
2024-03-14 07:55:18 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,24 +1,24 @@
 public abstract class Item {
-        public String name;
-        private String description;
+    public String name;
+    private String description;
 
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        protected String ID;
-        int price;
-
-        public abstract void showInfo();
-
-        public Item(String name, String description, String ID, int price) {
-            this.name = name;
-            this.description = description;
-            this.ID = ID;
-            this.price = price;
-        }
+    public String getDescription() {
+        return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    protected String ID;
+    int price;
+
+    public abstract void showInfo();
+
+    public Item(String name, String description, String ID, int price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+}
