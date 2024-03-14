fc3f49e3ec33ee3df2df254ed7ecb5ced2d811db
Ngo Ha Phuong <121754520+chophinho@users.noreply.github.com>
2024-03-14 07:47:22 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,24 @@
-public class Item {
-}
+public static abstract class Item {
+        public String name;
+        private String description;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        protected String ID;
+        int price;
+
+        public abstract void showInfo();
+
+        public Item(String name, String description, String ID, int price) {
+            this.name = name;
+            this.description = description;
+            this.ID = ID;
+            this.price = price;
+        }
+    }
