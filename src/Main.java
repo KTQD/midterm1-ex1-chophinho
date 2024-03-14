1f3314e36d476bcb34d46d4e507c73ca3d641552
Ngo Ha Phuong <121754520+chophinho@users.noreply.github.com>
2024-03-14 07:48:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
-        mp3.showInfo();
+            MP3 mp3 = new MP3("Em la ngoai le cua anh","blava","p12",12,"4p23");
+            mp3.showInfo();
 
-        Book book;
-        book.showInfo();
+            Book book = new Book("Bai hoc nho doi","nafsge","p12",12,"Tinh", 123,"21+");
+            book.showInfo();
 
     }
 }
