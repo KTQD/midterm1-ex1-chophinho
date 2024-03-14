21940eaaa41d4013c2e90524172052084527c8f0
Ngo Ha Phuong <121754520+chophinho@users.noreply.github.com>
2024-03-14 07:47:45 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,18 @@
-public class MP3 {
-}
+public static class MP3 extends Item {
+        public String duration;
+
+        public MP3(String name, String description, String ID, int price, String duration) {
+            super(name, description, ID, price);
+            this.duration = duration;
+        }
+
+        public void showInfo() {
+            System.out.println("Song name: " + name);
+            System.out.println("Song description: " + getDescription());
+            System.out.println("Song ID: " + ID);
+            System.out.println("Song price: " + price);
+            System.out.println("Song duration: " + duration);
+
+        }
+
+    }
